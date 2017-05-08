3582e682942f16c4c36d358137e234bacaf8235e
won <wjin0352@gmail.com>
2017-05-08 19:22:35 -0400

    added sample reducer and firebase setup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    // Initialize Firebase
+    const config = {
+      apiKey: 'AIzaSyAsDtmDoIL-cPYgxTtq4OB-DBanBw7D7Vw',
+      authDomain: 'manager-7996b.firebaseapp.com',
+      databaseURL: 'https://manager-7996b.firebaseio.com',
+      projectId: 'manager-7996b',
+      storageBucket: 'manager-7996b.appspot.com',
+      messagingSenderId: '37145095904'
+    };
+    
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
       <View>
         <Text>
           Hello
