b50c2429ebb18c280f3620a765b7c4171cdcd47b
won <wjin0352@gmail.com>
2017-05-12 23:59:22 -0400

    router scene nested routes

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
   return (
-    <Router>
-      <Scene key="login" component={LoginForm} title="Please Login" initial/>
+    <Router sceneStyle={{ paddingTop: 60 }}>
+      <Scene key="auth">
+        <Scene key="login" component={LoginForm} title="Please Login" />
+      </Scene>
+      
+      <Scene key="main">
+        <Scene key="employeeList" component={EmployeeList} title="Employees" />
+      </Scene>
     </Router>
   );
 };
---
+++ src/components/EmployeeList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeList extends Component {
+  render() {
+    return (
+      <View> 
+        <Text>Employee list</Text>
+        <Text>Employee list</Text>
+        <Text>Employee list</Text>
+        <Text>Employee list</Text>
+        <Text>Employee list</Text>
+      </View>
+    );
+  }
+}
+
+export default EmployeeList;
