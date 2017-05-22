44cf399c6f4f4aa60b965029c90dc419306b4957
won <wjin0352@gmail.com>
2017-05-22 18:18:52 -0400

    resuable employee create form

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,8 +1,9 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { ListView, View, Text } from 'react-native';
+import { ListView } from 'react-native';
 import { employeesFetch } from '../actions';
+import ListItem from './ListItem';
 
 class EmployeeList extends Component {
   componentWillMount() {
@@ -26,12 +27,18 @@ class EmployeeList extends Component {
     this.dataSource = ds.cloneWithRows(employees);
   }
 
+  renderRow(employee) {
+    return <ListItem employee={employee} />;
+  }
+
   render() {
     console.log(this.props);
     return (
-      <View> 
-        <Text></Text>
-      </View>
+      <ListView 
+        enableEmptySections
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
     );
   }
 }
@@ -44,4 +51,4 @@ const mapStateToProps = state => {
    return { employees };
 };
 
-export default connect(mapStateToProps, {employeesFetch})(EmployeeList);
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
